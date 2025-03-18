dd324e066fbcc8514e3c8f6294be1c74f3b18a22
wesleysouza <wesleyosou@hotmail.com>
2025-03-18 20:52:43 -0300

    fix: name of subnet resources

--- paramaters_store.tf
+++ paramaters_store.tf
@@ -40,19 +40,19 @@ resource "aws_ssm_parameter" "public_1c" {
   value = aws_subnet.public_subnet_1c.id
 }
 
-resource "aws_ssm_parameter" "database_1a" {
+resource "aws_ssm_parameter" "databases_1a" {
   name  = format("/%s/vpc/subnet_databases_1a", var.project_name)
   type  = "String"
   value = aws_subnet.databases_subnet_1a.id
 }
 
-resource "aws_ssm_parameter" "database_1b" {
+resource "aws_ssm_parameter" "databases_1b" {
   name  = format("/%s/vpc/subnet_databases_1b", var.project_name)
   type  = "String"
   value = aws_subnet.databases_subnet_1b.id
 }
 
-resource "aws_ssm_parameter" "database_1c" {
+resource "aws_ssm_parameter" "databases_1c" {
   name  = format("/%s/vpc/subnet_databases_1c", var.project_name)
   type  = "String"
   value = aws_subnet.databases_subnet_1c.id
